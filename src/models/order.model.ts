cd57def3288e40cd2cf55b84dbd6aa15881b9c91
Juliana Giusti <juliana.vmelo@gmail.com>
2022-08-08 06:06:21 -0300

    lint

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -11,14 +11,14 @@ export default class OrderModel {
   public async getAll(): Promise<Order[]> {
     // para agregar conjunto de resultados em uma única matriz json https://dev.mysql.com/doc/refman/5.7/en/aggregate-functions.html#function_json-arrayagg
     const result = await this.connection.execute(
-        `SELECT o.id AS id,
-        o.userId AS userId,
-        JSON_ARRAYAGG(p.id) AS productsIds
-        FROM Trybesmith.Orders AS o
-        INNER JOIN Trybesmith.Products AS p
-        ON o.id = p.orderId
-        group by id
-        order by userId;`,
+      `SELECT o.id AS id,
+      o.userId AS userId,
+      JSON_ARRAYAGG(p.id) AS productsIds
+      FROM Trybesmith.Orders AS o
+      INNER JOIN Trybesmith.Products AS p
+      ON o.id = p.orderId
+      group by id
+      order by userId;`,
     );
     const [rows] = result;
     return rows as Order[];
